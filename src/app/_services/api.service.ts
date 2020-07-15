55b708151910e894571a464c4ee27b0b3d5823cb
Kadermiyanyedi <Kadermiyanyedi@hotmail.com>
2020-07-15 03:13:03 +0300

    added service

---
+++ src/app/_services/api.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private getUrl = 'https://opentdb.com/api.php?amount=10&category=18&type=multiple'
+
+  constructor(private _http: HttpClient) { }
+
+  getQuestion() {
+    return this._http.get(this.getUrl).subscribe(data => {
+      for (var i = 0; i < data['results'].length; i++) {
+        var random = Math.floor(Math.random() * 3)
+        var correctAnswer = data['results'][i].correct_answer;
+
+        //We put the correct answer in a random index
+        var tmp = data['results'][i]['incorrect_answers'][random]
+        data['results'][i]['incorrect_answers'][random] = correctAnswer
+        data['results'][i]['incorrect_answers'].push(tmp)
+        localStorage.setItem('questions', JSON.stringify(data))
+
+        // console.log(data['results'][this.index])
+        // console.log(data['results'].length)
+      }
+
+    },
+      err => console.error(err)
+    )
+  }
+}
